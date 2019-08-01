1320b51e1fd1d9b6c21504f691097300f6000bee
smirad91 <smirad91@gmail.com>
2019-08-01 13:35:43 +0200

    Update

--- Lib/common/DriverData.py
+++ Lib/common/DriverData.py
@@ -48,8 +48,7 @@ class DriverData:
         if platform.system() in "Windows":
             from ctypes import windll
             if windll.user32.BlockInput(True) == 0:
-                pass
-               #raise Exception("Not running script as admin")
+                raise Exception("Not running script as admin")
         if is_forwarded("browser") is not None:
             driver = is_forwarded("browser")
         DriverData.driverName = driver
